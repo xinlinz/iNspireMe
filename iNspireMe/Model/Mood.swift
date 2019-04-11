a6698c484ce3caf6aacc23b3fe2d737f33818f3f
xinlinzhou <xinlinzhou@berkeley.edu>
2019-04-10 20:10:06 -0700

    added mood

---
+++ iNspireMe/Model/Mood.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  iNspireMe
+//
+//  Created by Xinlin Zhou on 4/10/19.
+//  Copyright © 2019 Xinlin Zhou. All rights reserved.
+//
+
+import Foundation
+class Mood {
+    var mood: String!
+    var quotes = [Quote]()
+    
+    init(mood: String){
+        self.mood = mood
+    }
+}
