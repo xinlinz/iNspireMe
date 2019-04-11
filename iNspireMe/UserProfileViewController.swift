0efc5dde4afc982efd1bcb32d331de523aed4f48
xinlinzhou <xinlinzhou@berkeley.edu>
2019-04-10 17:38:38 -0700

    implementing user profile

---
+++ iNspireMe/UserProfileViewController.swift
@@ -0,0 +1,31 @@
+//
+//  UserProfileViewController.swift
+//  iNspireMe
+//
+//  Created by Xinlin Zhou on 4/10/19.
+//  Copyright © 2019 Xinlin Zhou. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBOutlet weak var
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
